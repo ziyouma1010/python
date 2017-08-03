8688e24ec9ca314cf67bff8976fe0b9db790d49b
ziyouma1010 <ziyouma1010@qq.com>
2017-08-03 19:58:56 +0800

    Add files via upload

---
+++ ex9.py
@@ -0,0 +1,14 @@
+# here's some new strange stuff,remember type it exactly.
+
+days = "Mon Tue wed thu fri sat sun"
+months = "jan\nfeb\nmar\napr\nmay\njun\njul\naug"
+
+print "here are the days:",days
+print "here are the months:",months
+
+print """
+there's something going on here.
+with the three double-quotes.
+we'll be able to type as much as we like.
+even 4 lines if we want,or 5,or 6.
+"""

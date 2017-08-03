82ee78e20500dd93b9d010c969e333c47afc0d85
ziyouma1010 <ziyouma1010@qq.com>
2017-08-03 17:13:46 +0800

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,9 @@
+# a comment,this is so you can read your programlater.
+# anything after the # is ignored by pyhthon.
+
+print "i could have code like this." # and the comment after is ignored
+
+# you can also use a comment to "disable" or comment out a piece of code:
+# print "this won't run."
+
+print "this will run."
